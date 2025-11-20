6d5534c378970b6362d1712ad84898bbf2e53202
DeepFuture <12919034+gongkaiming@user.noreply.gitee.com>
2025-11-20 15:24:22 +0800

    recommit reset.php

---
+++ reset.php
@@ -0,0 +1,16 @@
+<?php
+// 创建三维数组
+$myarray = array(
+    array(
+        array(1, 2),
+        array(3, 4),
+    ),
+    array(
+        array(5, 6),
+        array(7, 8),
+    ),
+);
+      
+// 输出数组信息
+print_r($myarray);
+?>
